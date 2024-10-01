3c2185ef4bec4336194322d5329f14ef06a39de7
Elson Chan <elsonccjgmail.com>
2024-10-01 14:47:04 +0800

    Added pytest for dto classes

---
+++ unit_tests/src/models/dto_data_classes/test_extracted_search_result_dto.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from uuid import UUID
+
+import pytest
+from src.models.dto_data_classes.extracted_search_result_dto import ExtractedSearchResultDTO
+from src.models.extractor_data_classes.extracted_search_result import ExtractedSearchResult
+
+
+dummy_uuid: UUID = UUID("12345678123456781234567812345678")
+
+
+class TestExtractedSearchResultDTO:
+    @pytest.mark.parametrize(
+        ["search_result", "jobs_id", "expected_result"],
+        [
+            [
+                ExtractedSearchResult(
+                    id=str(dummy_uuid),
+                    user_id="test_user_id",
+                    url="http://example.com",
+                    date="2024-01-01",
+                    body="Test body content",
+                    created_at=datetime(2024, 10, 1, 12, 0, 0),
+                ),
+                "test_jobs_id",
+                ExtractedSearchResultDTO(
+                    id=str(dummy_uuid),
+                    jobs_id="test_jobs_id",
+                    user_id="test_user_id",
+                    url="http://example.com",
+                    date="2024-01-01",
+                    body="Test body content",
+                    created_at=datetime(2024, 10, 1, 12, 0, 0),
+                ),
+            ],
+            [
+                ExtractedSearchResult(
+                    id=str(dummy_uuid),
+                    user_id="test_user_id",
+                    url="http://example.com",
+                    date="2024-01-01",
+                    body=None,
+                    created_at=datetime(2024, 10, 1, 12, 0, 0),
+                ),
+                "test_jobs_id",
+                ExtractedSearchResultDTO(
+                    id=str(dummy_uuid),
+                    jobs_id="test_jobs_id",
+                    user_id="test_user_id",
+                    url="http://example.com",
+                    date="2024-01-01",
+                    body=None,
+                    created_at=datetime(2024, 10, 1, 12, 0, 0),
+                ),
+            ],
+        ],
+    )
+    def test_from_search_results(
+        self, search_result: ExtractedSearchResult, jobs_id: str, expected_result: ExtractedSearchResultDTO
+    ) -> None:
+        # Act
+        extracted_search_result_dto = ExtractedSearchResultDTO.from_search_results(search_result, jobs_id)
+
+        # Assert
+        assert extracted_search_result_dto == expected_result
---
+++ unit_tests/src/models/dto_data_classes/test_raw_search_result_dto.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from uuid import UUID
+
+import pytest
+from freezegun import freeze_time
+from unittest.mock import patch
+from src.models.dto_data_classes.raw_search_results_dto import RawSearchResultsDTO
+
+
+dummy_uuid: UUID = UUID("12345678123456781234567812345678")
+
+
+class TestRawSearchResultsDTO:
+    @pytest.mark.parametrize(
+        ["user_id", "search_term", "result", "expected_result"],
+        [
+            [
+                "dummy_user_id",
+                "starbucks",
+                "Matcha Latte",
+                RawSearchResultsDTO(
+                    id=str(dummy_uuid),
+                    user_id="dummy_user_id",
+                    search_term="starbucks",
+                    result="Matcha Latte",
+                    created_at=datetime(year=2024, month=10, day=1, hour=12)
+                )
+            ],
+            [
+                "dummy_user_id",
+                "starbucks",
+                None,
+                RawSearchResultsDTO(
+                    id=str(dummy_uuid),
+                    user_id="dummy_user_id",
+                    search_term="starbucks",
+                    result=None,
+                    created_at=datetime(year=2024, month=10, day=1, hour=12)
+                )
+            ],
+        ],
+    )
+    def test_create(
+            self,
+            user_id: str,
+            search_term: str,
+            result: str | None,
+            expected_result: RawSearchResultsDTO
+    ) -> None:
+        # freezetime to freeze the actual create method with datetime.utcnow()
+        with freeze_time("2024-10-1 12:00:00"), patch(
+                "src.models.dto_data_classes.raw_search_results_dto.uuid.uuid4", return_value=dummy_uuid
+        ):
+            raw_search_results_dto: RawSearchResultsDTO = RawSearchResultsDTO.create(
+                user_id=user_id, search_term=search_term, result=result
+            )
+            assert raw_search_results_dto == expected_result
