86ce6d2bda7399ce12d3dea7ddd8eb4b07c3e070
Elson Chan <elsonccj@gmail.com>
2025-06-30 19:54:37 +0800

    black ruff

--- unit_tests/src/services/extractors/test_bs4_extractor.py
+++ unit_tests/src/services/extractors/test_bs4_extractor.py
@@ -6,7 +6,9 @@ import pytest
 from freezegun import freeze_time
 
 from src.services.search_result_extractor import BS4SearchResultExtractor
-from src.models.extractor_data_classes.extracted_search_result import ExtractedSearchResult
+from src.models.extractor_data_classes.extracted_search_result import (
+    ExtractedSearchResult,
+)
 
 """
 High Level: We want to test the extract method on various HTML structures.
@@ -36,10 +38,10 @@ class TestBS4SearchResultExtractor:
 
     @pytest.mark.parametrize(
         ("html", "expected_search_results"),
-    [
-        # Case 1: Multiple Search Results
-        (
-            """
+        [
+            # Case 1: Multiple Search Results
+            (
+                """
             <div>
                 <div>
                     <a href="link1">Link 1</a>
@@ -52,28 +54,28 @@ class TestBS4SearchResultExtractor:
                 </div>
             </div>
             """,
-            [
-                ExtractedSearchResult(
-                    id="dummy_id",
-                    user_id="dummy_user_id",
-                    url="link1",
-                    date="Date 1",
-                    body="Body 1",
-                    created_at=datetime(2024, 10, 1, 12)
-                ),
-                ExtractedSearchResult(
-                    id="dummy_id",
-                    user_id="dummy_user_id",
-                    url="link2",
-                    date="Date 2",
-                    body="Body 2",
-                    created_at=datetime(2024, 10, 1, 12)
-                ),
-            ]
-        ),
-        # Case 2: Multiple Search Results with some info_count < 2
-        (
-            """
+                [
+                    ExtractedSearchResult(
+                        id="dummy_id",
+                        user_id="dummy_user_id",
+                        url="link1",
+                        date="Date 1",
+                        body="Body 1",
+                        created_at=datetime(2024, 10, 1, 12),
+                    ),
+                    ExtractedSearchResult(
+                        id="dummy_id",
+                        user_id="dummy_user_id",
+                        url="link2",
+                        date="Date 2",
+                        body="Body 2",
+                        created_at=datetime(2024, 10, 1, 12),
+                    ),
+                ],
+            ),
+            # Case 2: Multiple Search Results with some info_count < 2
+            (
+                """
             <div>
                 <div>
                     <a href="link1">Link 1</a>
@@ -83,20 +85,18 @@ class TestBS4SearchResultExtractor:
                 </div>
             </div>
             """,
-            []
-        ),
-    ]
+                [],
+            ),
+        ],
     )
     def test_extract(
-            self,
-            extractor,
-            html,
-            expected_search_results: list[ExtractedSearchResult] | None
+        self,
+        extractor,
+        html,
+        expected_search_results: list[ExtractedSearchResult] | None,
     ) -> None:
         # Call the extract method with the given HTML content
         results = extractor.extract(html, "dummy_id")
 
         # Assertions to verify the results
         assert results == expected_search_results
-
-
